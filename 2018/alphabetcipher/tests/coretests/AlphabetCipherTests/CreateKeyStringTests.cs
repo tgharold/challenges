cf0bcddecc5b9c2f83acd0bb23c3b890033226c5
Thomas Harold <tgharold@gmail.com>
2018-06-19 19:49:58 -0400

    Put file in proper folder, add test cases

--- 2018/alphabetcipher/tests/coretests/AlphabetCipherTests/CreateKeyStringTests.cs
+++ 2018/alphabetcipher/tests/coretests/AlphabetCipherTests/CreateKeyStringTests.cs
@@ -1,16 +1,17 @@
 using core;
 using Xunit;
 
-namespace coretests
+namespace coretests.AlphabetCipherTests
 {
     public class CreateKeyString
     {
-        private readonly AlphabetCipher _sut = new AlphabetCipher("test");
+        private readonly AlphabetCipher _sut = new AlphabetCipher();
 
         [Theory]
         [InlineData("abx", 3, "abx")]
         [InlineData("xyz", 5, "xyzxy")]
         [InlineData("key", 6, "keykey")]
+        [InlineData("snitch", 26, "snitchsnitchsnitchsnitchsn")]
         public void Create_correct_key_string(string key, int length, string expected)
         {
             var result = _sut.CreateKeyString(key, length);
