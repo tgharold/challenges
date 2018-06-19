c8775fa2390a50773a313288bf8f48a33cbab292
Thomas Harold <tgharold@gmail.com>
2018-06-19 17:33:23 -0400

    multi-project dotnet solution

---
+++ 2018/alphabetcipher/decode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace decode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
