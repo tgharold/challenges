76b13f6e17b331fb2472b549e7a754abfe120a17
Thomas Harold <tgharold@gmail.com>
2018-06-18 21:53:27 -0400

    add more complex test examples

--- 2018/diceroller/DiceParserTests.cs
+++ 2018/diceroller/DiceParserTests.cs
@@ -24,10 +24,32 @@ namespace diceroller
         [InlineData("2d6", 2)]
         [InlineData("3d8", 3)]
         [InlineData("4d20", 4)]
-        public void Returns_the_correct_count_of_dice(string input, int expectedCount)
+        [InlineData("13d20", 13)]
+        [InlineData("25d4", 25)]
+        public void Returns_the_correct_count_of_dice_for_simple_strings(
+            string input, 
+            int expectedCount
+            )
         {
             var result = DiceParser.GetDice(input);
             Assert.Equal(expectedCount, result.Count());
         }
+
+        [Theory]
+        [InlineData("1d4 2d6", 3)]
+        [InlineData("2d6\n3d8", 5)]
+        [InlineData("25d4,3d8", 28)]
+        [InlineData("4d20, 6d10, 5d8", 15)]
+        [InlineData("13d20\t2d8", 15)]
+        [InlineData("1d6\n25d4\t4d3\r5d6", 35)]
+        public void Returns_the_correct_count_of_dice_for_complex_strings(
+            string input, 
+            int expectedCount
+            )
+        {
+            var result = DiceParser.GetDice(input);
+            Assert.Equal(expectedCount, result.Count());
+        }
+
     }
 }
