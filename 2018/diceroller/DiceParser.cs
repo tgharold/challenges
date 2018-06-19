f7ecb8086fe293646f65d504b7c7a16c67a9fb6b
Thomas Harold <tgharold@gmail.com>
2018-06-18 21:49:44 -0400

    create a DieParser class

---
+++ 2018/diceroller/DiceParser.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace diceroller
+{
+    public static class DiceParser
+    {
+        public static IEnumerable<Die> GetDice(string dice)
+        {
+            var result = new List<Die>();
+            return result;
+        }
+    }
+}
