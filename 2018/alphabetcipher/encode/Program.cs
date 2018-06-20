52bc6be9f7d587ebc9644cbabe0bc4a607ab6ddd
Thomas Harold <tgharold@gmail.com>
2018-06-19 20:05:23 -0400

    implement encode console application

--- 2018/alphabetcipher/encode/Program.cs
+++ 2018/alphabetcipher/encode/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Linq;
+using core;
 
 namespace encode
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Count() < 2)
+            {
+                Console.WriteLine("Usage: encode key input");
+                return 1;
+            }
+
+            var key = args[0].ToLower();
+            var input = args[1].ToLower();
+            var cipher = new AlphabetCipher();
+            Console.WriteLine(cipher.Encode(input, key));
+
+            return 0;
         }
     }
 }
