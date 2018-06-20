00ca0b726b457d40ec256f746f102843ee97d5ce
Thomas Harold <tgharold@gmail.com>
2018-06-19 20:30:22 -0400

    implement decode console app

--- 2018/alphabetcipher/decode/Program.cs
+++ 2018/alphabetcipher/decode/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Linq;
+using core;
 
 namespace decode
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Count() < 2)
+            {
+                Console.WriteLine("Usage: decode key input");
+                return 1;
+            }
+
+            var key = args[0].ToLower();
+            var input = args[1].ToLower();
+            var cipher = new AlphabetCipher();
+            Console.WriteLine(cipher.Decode(input, key));
+
+            return 0;
         }
     }
 }
