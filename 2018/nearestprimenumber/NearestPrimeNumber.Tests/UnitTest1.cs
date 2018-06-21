a1e6b786ac8191b4c27df73a3e8dc38872ef8fda
Thomas Harold <tom.harold@ritterim.com>
2018-06-20 20:24:00 -0400

    setup .NET core solution

---
+++ 2018/nearestprimenumber/NearestPrimeNumber.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace NearestPrimeNumber.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ 2018/nearestprimenumber/NearestPrimeNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NearestPrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
