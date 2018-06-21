ba8e868e0b9f3c57da865cc99336cb90c60af310
Thomas Harold <tom.harold@ritterim.com>
2018-06-20 20:31:57 -0400

    create AKS primality class

---
+++ 2018/nearestprimenumber/NearestPrimeNumber/AgrawalKayalSaxenaPrimalityCalculator.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace NearestPrimeNumber
+{
+    public class AgrawalKayalSaxenaPrimalityCalculator : IPrimalityCalculator
+    {
+        // https://en.wikipedia.org/wiki/AKS_primality_test
+        public bool IsPrime(long input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 2018/nearestprimenumber/NearestPrimeNumber/IPrimalityCalculator.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace NearestPrimeNumber
+{
+    public interface IPrimalityCalculator
+    {
+        bool IsPrime(long input);
+    }
+}
