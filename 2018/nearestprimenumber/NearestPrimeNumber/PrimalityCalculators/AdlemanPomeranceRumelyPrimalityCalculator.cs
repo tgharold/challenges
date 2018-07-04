9cb43424dcb30a61b5da11bbab60fdab7f81da64
Thomas Harold <tom.harold@ritterim.com>
2018-07-04 08:34:11 -0400

    Look into using APR-CL method

---
+++ 2018/nearestprimenumber/NearestPrimeNumber/PrimalityCalculators/AdlemanPomeranceRumelyPrimalityCalculator.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace NearestPrimeNumber.PrimalityCalculators
+{
+    ///<summary>
+    ///<para></para>
+    ///</summary>
+    public class AdlemanPomeranceRumelyCohenLenstraPrimalityCalculator : IPrimalityCalculator
+    {
+        // https://en.wikipedia.org/wiki/Adleman%E2%80%93Pomerance%E2%80%93Rumely_primality_test
+        public bool IsPrime(long input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
