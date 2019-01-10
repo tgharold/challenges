7c2cfccc739cf4af99bd5c0b18b36fbd14e14406
Thomas Harold <tgharold@gmail.com>
2019-01-10 06:33:51 -0500

    example from documentation

--- 2019/qml-net-test1/src/QmlTest1/Program.cs
+++ 2019/qml-net-test1/src/QmlTest1/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using Qml.Net;
 
 namespace QmlTest1
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var app = new QGuiApplication(args))
+            {
+                using (var engine = new QQmlApplicationEngine())
+                {
+                    // TODO: Register your .NET types.
+                    // Qml.RegisterType<NetObject>("test");
+
+                    engine.Load("Main.qml");
+                    
+                    return app.Exec();
+                }
+            }
         }
     }
 }
