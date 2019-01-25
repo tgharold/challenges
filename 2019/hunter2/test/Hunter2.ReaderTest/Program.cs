fcb4ef225ac15869178595366dc4330ee8907e4f
Thomas Harold <tgharold@gmail.com>
2019-01-24 19:26:33 -0500

    reader test app

---
+++ 2019/hunter2/test/Hunter2.ReaderTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hunter2.ReaderTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
