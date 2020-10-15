37acaa3420203f0eb2b6b75ed0dded67d7b8f1bc
Thomas Harold <tgharold@gmail.com>
2020-10-14 22:39:59 -0400

    create TransitionPoints

---
+++ 2020/transition-points/TransitionPoints/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TransitionPoints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
